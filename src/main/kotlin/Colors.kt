5c4292688f5f17d308fcd9383090c57101552ccf
MaraKa.exe <94853428+MarcASO1560@users.noreply.github.com>
2024-01-12 09:36:08 +0100

    Primeros pasos del main

---
+++ src/main/kotlin/Colors.kt
@@ -0,0 +1,20 @@
+import androidx.compose.ui.graphics.Color
+
+
+//DARK THEME
+val Dark2 = Color(0xff444444)
+val Dark1 = Color(0xff222222)
+
+//BRIGHT THEME
+val Bright2 = Color(0xffcccccc)
+val Bright1 = Color(0xffeeeeee)
+
+//COLORS APP IDENTIFICATORS
+val LightBlue = Color(0xff55bce5)
+val Turquoise = Color(0xff418c84)
+val Lilac      = Color(0xff8e64d5)
+
+//TEXT COLORS
+val Grey  = Color(0xff888888)
+val Black = Color(0xff000000)
+val White = Color(0xffffffff)
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,31 +1,129 @@
-import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.*
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterStart
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Tray
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-
-@Composable
-@Preview
-fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+fun main() = application {
+    val icon = painterResource("drawable/PearlTeethIcon.png")
+    Tray(
+        icon = icon,
+        menu = {
+            Item("Quit App", onClick = ::exitApplication)
         }
+    )
+    Window(onCloseRequest = ::exitApplication, title = "PearlTeeth", icon = icon) {
+        Scaffold(
+            topBar =
+            {
+                Column {
+                    TopBar()
+                    Row {
+                        SideBar()
+                        Logo()
+                    }
+                }
+            },
+            content = { padding ->
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(padding),
+                    color = Bright1
+                ){
+
+                }
+            }
+        )
     }
 }
 
-fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
-        App()
+@Composable
+fun SideBar() {
+    MaterialTheme {
+       Box(modifier = Modifier
+           .shadow(10.dp)
+           .fillMaxHeight()
+           .width(100.dp)
+           .background(Bright2)
+           .padding(end = 10.dp)
+       )
     }
+}
+@Composable
+fun Logo() {
+    val image = painterResource("drawable/PearlTeethIcon.png")
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center),
+
+    ) {
+        Row(modifier = Modifier.padding(20.dp), verticalAlignment = Alignment.CenterVertically) {
+            Image(
+                painter = image,
+                alpha = 0.75F,
+                contentDescription = "Icon",
+                modifier = Modifier
+                    .size(140.dp)
+                    .padding(20.dp)
+                    .align(Alignment.CenterVertically)
+            )
+            Text("PearlTeeth",
+                color = Grey,
+                fontSize = 40.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .wrapContentSize(align = CenterStart)
+            )
+
+        }
+    }
+}
+@Composable
+fun TopBar() {
+    TopAppBar(
+        modifier = Modifier
+            .shadow(10.dp),
+        backgroundColor = Turquoise,
+        contentColor = White,
+        title = {},
+        navigationIcon = {
+            IconButton(onClick = {}) {
+                Icon(
+                    Icons.Filled.KeyboardArrowLeft,
+                    contentDescription = "arrow1"
+                )
+            }
+            IconButton(onClick = {}) {
+                Icon(
+                    Icons.Filled.KeyboardArrowRight,
+                    contentDescription = "arrow2"
+                )
+            }
+            IconButton(onClick = {}) {
+                Icon(
+                    Icons.Filled.Home,
+                    contentDescription = "home"
+                )
+            }
+        },
+    )
 }
